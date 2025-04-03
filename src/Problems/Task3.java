4963cff819d73963231694adfaff2837e5b3b00d
kunmenaiym <fxolzet19@gmail.com>
2025-04-03 11:12:04 +0500

    task 3

--- src/Problems/Task3.java
+++ src/Problems/Task3.java
@@ -4,19 +4,41 @@ import java.util.Scanner;
 
 public class Task3 {
 
+    // Starts the program by reading user input, measuring execution time
     public static void run (){
 
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
 
+        // Check if the number is prime or composite
         if (isPrime(n, 2)){
             System.out.println("Prime");
         }
         else {
             System.out.println("Composite");
         }
+
+        // Measuring execution time and output results
+        double startTime = System.nanoTime();
+        boolean result = isPrime(n,  2);
+        double endTime = System.nanoTime();
+        double duration = (endTime - startTime) / 1000000;
+        System.out.println(result);
+        System.out.println("Time taken: " + duration + " milliseconds");
     }
 
+
+    /**
+     * This method checks whether the given number is prime using recursion.
+     * It checks if the number is divisible by any number starting from 2 up to the square root of the number.
+     *
+     * Time complexity: O(sqrt(n)), where n is the number being checked for primality.
+     * The method checks for divisibility from 2 to the square root of n, which results in a time complexity of O(sqrt(n)).
+     *
+     * @param n The number to be checked for primality.
+     * @param i The current divisor being tested (starts from 2 and increments).
+     * @return true if n is prime, false if n is not prime.
+     */
     public static boolean isPrime(int n, int i){
 
         if (n < 2) return false;
