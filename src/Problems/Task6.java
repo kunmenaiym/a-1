47edee65df001011e3b332d6fa8f19108d013191
kunmenaiym <fxolzet19@gmail.com>
2025-04-03 21:02:37 +0500

    task 6

--- src/Problems/Task6.java
+++ src/Problems/Task6.java
@@ -4,15 +4,34 @@ import java.util.Scanner;
 
 public class Task6 {
 
-    public static void run() {
-        Scanner in = new Scanner(System.in);
+    // This method runs the task
+    public static void run(Scanner in) {
+
         int a = in.nextInt();
         int n = in.nextInt();
 
-        System.out.println(pow(a, n));
+        // Measuring execution time and output results
+        double startTime = System.nanoTime();
+        int result = pow(a, n);
+        double endTime = System.nanoTime();
+        double duration = (endTime - startTime) / 1000000;
+        System.out.println(result);
+        System.out.println("Time taken: " + duration + " milliseconds");
     }
 
-    public static int pow(int n, int a) {
+
+    /**
+     * This method calculates the power of a number using recursion.
+     * It computes a^n, where a is the base and n is the exponent.
+     *
+     * Time complexity: O(n), where n is the exponent.
+     * The method performs n recursive calls, making it linear in complexity.
+     *
+     * @param a The base number.
+     * @param n The exponent.
+     * @return The result of a raised to the power of n.
+     */
+    public static int pow(int a, int n) {
         if (n == 0) return 1;
 
         if (n == 1) return a;
