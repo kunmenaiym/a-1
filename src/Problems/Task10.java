2ad7bc7606b788a7513b7c292caf689774dbdb7a
kunmenaiym <fxolzet19@gmail.com>
2025-04-03 21:05:38 +0500

    task 10

--- src/Problems/Task10.java
+++ src/Problems/Task10.java
@@ -3,14 +3,33 @@ package Problems;
 import java.util.Scanner;
 
 public class Task10 {
-    public static void run() {
-        Scanner in = new Scanner(System.in);
+
+    // This method runs the task
+    public static void run(Scanner in) {
+
         int a = in.nextInt();
         int b = in.nextInt();
 
-        System.out.println(gcd(a, b));
+        // Measuring execution time and output results
+        double startTime = System.nanoTime();
+        int result = gcd(a, b);
+        double endTime = System.nanoTime();
+        double duration = (endTime - startTime) / 1000000;
+        System.out.println(result);
+        System.out.println("Time taken: " + duration + " milliseconds");
     }
 
+    /**
+     * This method calculates the greatest common divisor (GCD) of two numbers using
+     * the Euclidean algorithm.
+     *
+     * Time complexity: O(log(min(a, b))), which is the number of divisions required
+     * to reduce the smaller number to zero.
+     *
+     * @param a The first number.
+     * @param b The second number.
+     * @return The greatest common divisor (GCD) of a and b.
+     */
     public static int gcd(int a, int b) {
         if (b == 0) return a;
         return gcd(b, a % b);
