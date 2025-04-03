e8eb804a4803ac95232707d10293e6764483bb0b
kunmenaiym <fxolzet19@gmail.com>
2025-04-03 21:04:21 +0500

    task 8

--- src/Problems/Task8.java
+++ src/Problems/Task8.java
@@ -3,13 +3,33 @@ package Problems;
 import java.util.Scanner;
 
 public class Task8 {
-    public static void run() {
-        Scanner in = new Scanner(System.in);
+
+    // This method runs the task
+    public static void run(Scanner in) {
+
+
         String s = in.next();
 
+        // Measuring execution time and output results
+        double startTime = System.nanoTime();
+        String result = digitCheck(s, 0) ? "Yes" : "No";
+        double endTime = System.nanoTime();
+        double duration = (endTime - startTime) / 1000000;
+        System.out.println(result);
+        System.out.println("Time taken: " + duration + " milliseconds");
 
-        System.out.println(digitCheck(s, 0) ? "Yes" : "No" );
     }
+
+    /**
+     * This method recursively checks if a string consists only of digits.
+     *
+     * Time complexity: O(n), where n is the length of the string.
+     * The function checks each character once, resulting in linear complexity.
+     *
+     * @param s      The input string.
+     * @param index  The current index being checked.
+     * @return       true if the entire string consists only of digits, false otherwise.
+     */
 
     public static boolean digitCheck(String s, int index) {
         if (index == s.length()) {
